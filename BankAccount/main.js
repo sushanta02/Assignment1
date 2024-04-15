f410d2ddb419e445e2fcc5aefc0d8fb2994aaa9e
sushanta02 <sushanta02@gmail.com>
2024-04-15 17:50:17 +0600

    BankAccount updated 3

--- BankAccount/main.js
+++ BankAccount/main.js
@@ -1,8 +1,21 @@
 class BankAccount {
+  static accounts = [];
+
   constructor(accountNumber, ownerName, balance) {
+    if (BankAccount.accountExists(accountNumber)) {
+      throw new Error('Account number already exists');
+    }
+
     this.accountNumber = accountNumber;
     this.ownerName = ownerName;
     this.balance = balance;
+    console.log(`New Account ${this.accountNumber} created with initial balance $${balance}`);
+
+    BankAccount.accounts.push(this);
+  }
+
+  static accountExists(accountNumber) {
+    return BankAccount.accounts.some(account => account.accountNumber === accountNumber);
   }
 
   deposit(amount) {
@@ -31,9 +44,12 @@ class BankAccount {
 }
 
 
+
 const account1 = new BankAccount(1001, 'John Doe', 500);
+console.log(''); 
 const account2 = new BankAccount(1002, 'Jane Smith', 1000);
 
+console.log(''); 
 
 account1.deposit(200);
 account1.withdraw(100);
